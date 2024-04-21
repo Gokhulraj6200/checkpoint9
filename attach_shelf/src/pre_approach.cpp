42bc608417511a440a325b2dae3f3d4814a0c2f2
Gokhulraj6200 <gokhulraj6200@gmail.com>
2024-04-21 19:21:30 +0000

    add rotation logic to pre-approach

--- attach_shelf/src/pre_approach.cpp
+++ attach_shelf/src/pre_approach.cpp
@@ -1,4 +1,5 @@
 #include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include <cmath>
@@ -13,9 +14,15 @@ public:
 
     getting_params();
 
-    subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+
+    scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10,
         std::bind(&PreApproach::scan_callback, this,
+                  std::placeholders::_1));
+
+    odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "/odom", 10,
+        std::bind(&PreApproach::odom_callback, this,
                   std::placeholders::_1));
 
     publisher_ =
@@ -27,41 +34,93 @@ private:
     obstacle =
         this->get_parameter("obstacle").get_parameter_value().get<float>();
     degrees = this->get_parameter("degrees").get_parameter_value().get<float>();
+    target_yaw = degrees * M_PI / 180.0;
     RCLCPP_INFO(this->get_logger(), "Obstacle threshold set to: %f", obstacle);
     RCLCPP_INFO(this->get_logger(), "Rotation degrees set to: %f", degrees);
   }
 
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    float distance_to_obstacle = msg->ranges[msg->ranges.size() / 2];
-    RCLCPP_INFO(this->get_logger(), "Distance to obstacle: %f",
-                distance_to_obstacle);
 
-    if (distance_to_obstacle < obstacle) {
+    float distance_to_obstacle = msg->ranges[msg->ranges.size() / 2];
+
+    RCLCPP_INFO(this->get_logger(), "Distance to obstacle: %f", distance_to_obstacle);
+
+    if (distance_to_obstacle < obstacle) 
+    {
       RCLCPP_WARN(this->get_logger(), "Obstacle detected! Stopping robot.");
-      stop_robot();
-    } else {
+      obstacle_detected = true;
+    } 
+    else if (!obstacle_detected) 
+    {
       move_forward();
+    }
+  }
+
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+    double s = msg->pose.pose.orientation.w;
+    double x = msg->pose.pose.orientation.x;
+    double y = msg->pose.pose.orientation.y;
+    double z = msg->pose.pose.orientation.z;
+
+    // Calculate yaw angle from the quaternion
+    current_yaw = atan2(2.0 * (y * x + s * z), s * s + x * x - y * y - z * z);
+
+    if (obstacle_detected && !is_rotated) {
+      rotate_robot();
     }
   }
 
   void move_forward() {
     geometry_msgs::msg::Twist msg;
-    msg.linear.x = 0.25; 
+    msg.linear.x = 0.5; 
     publisher_->publish(msg);
     RCLCPP_INFO(this->get_logger(), "Moving forward");
   }
 
-  void stop_robot() {
+  void rotate_robot() {
+
+    double yaw_error = target_yaw - current_yaw;
+
+    if (yaw_error > M_PI) {
+      yaw_error -= 2 * M_PI;
+    } else if (yaw_error < -M_PI) {
+      yaw_error += 2 * M_PI;
+    }
+
+    RCLCPP_INFO(this->get_logger(),
+                "current_yaw: %f, target_yaw: %f, yaw_error: %f", current_yaw,
+                target_yaw, yaw_error);
+
+    if (abs(yaw_error) < yaw_tolerance) {
+      is_rotated = true;
+      angular_speed = 0.0;
+      RCLCPP_INFO(this->get_logger(),
+                  "Yaw error within tolerance, setting is_rotated to true");
+    } else {
+      angular_speed = kp * yaw_error;
+      RCLCPP_INFO(this->get_logger(), "Yaw error outside tolerance, rotating");
+    }
+
     geometry_msgs::msg::Twist msg;
-    msg.linear.x = 0.0; 
+    msg.linear.x = 0.0;
+    msg.angular.z = angular_speed;
     publisher_->publish(msg);
-    RCLCPP_INFO(this->get_logger(), "Stopping robot");
+
+    RCLCPP_INFO(this->get_logger(), "Rotating robot");
   }
 
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscriber_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscriber_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   float obstacle; 
   float degrees; 
+  float yaw_tolerance = 0.1;
+  float angular_speed = 0.0;
+  bool obstacle_detected = false;
+  bool is_rotated = false;
+  double current_yaw = 0.0;
+  double kp = 0.5;
+  double target_yaw = 0.0;
 };
 
 int main(int argc, char *argv[]) {
