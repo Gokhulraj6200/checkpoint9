008771f60bb75e442b3c19a6436dbb1964c259b8
Gokhulraj200 <gokhulraj200@gmail.com>
2024-04-29 12:33:20 +0000

    add attach_server

---
+++ my_components/include/my_components/attach_server.hpp
@@ -0,0 +1,72 @@
+#ifndef COMPOSITION_ATTACH_SERVER_HPP_
+#define COMPOSITION_ATTACH_SERVER_HPP_
+
+#include "my_components/visibility_control.h"
+#include "attach_shelf/srv/go_to_loading.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include <chrono>
+#include <cmath>
+#include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/empty.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <memory>
+
+namespace my_components {
+
+class AttachServe : public rclcpp::Node {
+public:
+  COMPOSITION_PUBLIC
+  explicit AttachServe(const rclcpp::NodeOptions &options);
+
+private:
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+      scan_subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
+  std::vector<double> msgvalue;
+  static const size_t NUM_LASER_READINGS = 1080;
+  std::vector<int> globalLegPos;
+  bool ready = false;
+  bool move_extra = false;
+  bool elevated = false;
+  int extratime = 8;
+  double angleLegs = 0.0;
+  double xDistance = 0.0;
+  double yDistance = 0.0;
+  double xDistance_once = 0.0;
+  double yDistance_once = 0.0;
+
+
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+  tf2::Quaternion cart_quat_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  geometry_msgs::msg::TransformStamped transform_stamped;
+
+  rclcpp::Service<attach_shelf::srv::GoToLoading>::SharedPtr approach_service_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> broadcaster;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr elevator_publisher;
+  
+  //methods
+  void handle_approach_request(
+      const std::shared_ptr<attach_shelf::srv::GoToLoading::Request> request,
+      std::shared_ptr<attach_shelf::srv::GoToLoading::Response> response);
+  void timer_callback();
+  void publish_cart_frame_transform();
+  void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+
+};
+
+} // namespace my_components
+
+#endif // COMPOSITION__SERVER_COMPONENT_HPP_
---
+++ my_components/src/manual_composition.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+
+#include "my_components/attach_server.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/executors/single_threaded_executor.hpp"
+#include "rclcpp/node_options.hpp"
+#include "rclcpp/utilities.hpp"
+#include <cstddef>
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+  // Force flush of the stdout buffer.
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+  rclcpp::init(argc, argv);
+
+  rclcpp::executors::SingleThreadedExecutor exec;
+  rclcpp::NodeOptions options;
+
+  auto attach_server = std::make_shared<my_components::MoveRobot>(options);
+  exec.add_node(attach_server);
+
+  // spin will block until work comes in, execute work as it becomes available,
+  // and keep blocking. It will only be interrupted by Ctrl-C.
+  exec.spin();
+
+  rclcpp::shutdown();
+
+  return 0;
+}
